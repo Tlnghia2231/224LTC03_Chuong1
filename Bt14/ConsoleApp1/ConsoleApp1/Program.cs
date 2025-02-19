f26f83343c51fc5af47831bab3e3fe2f3c4539b2
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-19 23:38:53 +0700

    Demo bai 14

---
+++ Bt14/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(result(2, 3));
+        }
+
+        private static double result(int x,int n)
+        {
+            int i = 0;
+            double S = 0;
+            while (i < n)
+            {
+                S += Math.Pow(x, 2 * i + 1);
+                i++;
+            }
+            return S;
+        }
+    }
+}
+//2 + 8 + 32
