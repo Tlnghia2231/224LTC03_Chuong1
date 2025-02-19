eb4edd22f301ee134d8a9fb956694e1501831401
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 00:03:06 +0700

    Demo bai 34

---
+++ Bt34/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(result(5));
+        }
+
+        private static double result(int n)
+        {
+            if (n > 0) return Math.Sqrt(n + result(n - 1));
+            else return 1;
+        }
+    }
+}
