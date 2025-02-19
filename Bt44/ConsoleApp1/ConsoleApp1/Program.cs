5fdc0be150fc6d14316b7b7988706f06627cd76b
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 00:05:20 +0700

    Demo bai 44

---
+++ Bt44/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(result(1234));
+        }
+
+        private static int result(int n)
+        {
+            int S = 0;
+            while (n > 0)
+            {
+                S += n % 10;
+                n /= 10;
+            }
+            return S;
+        }
+    }
+}
