07432b74ebbe26d0abfdd82838a7c52155808785
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-19 23:41:53 +0700

    Demo bai 24

---
+++ Bt24/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ung so le cua 555 : ");
+            printNumber(555);
+        }
+
+        private static void printNumber(int n)
+        {
+            int i = 1;
+            while (i <= n)
+            {
+                if ((n % i == 0) && ((i & 1) == 1))
+                {
+                    Console.Write(i + "\t");
+                }
+                i++;
+            }
+        }
+    }
+}
