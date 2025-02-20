01eeeae62042c256838a478a691bc225f8d96e59
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 22:28:48 +0700

    Demo bt 74

---
+++ Bt74/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(result(2, 4));
+        }
+
+        private static double result(int x, int n)
+        {
+            double S = 1;
+            int i = 0;
+            while (i <= n)
+            {
+                S += (Math.Pow(-1, i + 1) * Math.Pow(x, (2 * i + 1)) / Giaithua(2 * i + 1));
+                i++;
+            }
+            return S;
+        }
+        private static long Giaithua(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return n * Giaithua(n - 1);
+        }
+    }
+}
