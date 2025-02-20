27100f624c6c4a5cdbf6dadab1a077aea4c3d38d
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 20:25:21 +0700

    Hoan thanh bai 4

--- Bt4/ConsoleApp1/ConsoleApp1/Program.cs
+++ Bt4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,15 +2,17 @@
 {
     internal class Program
     {
-        private static int i = 1;
+        private static int i_231 = 1;
         static void Main(string[] args)
         {
-            Console.WriteLine(result(i, 4));
+            Console.Write("Nhap so n : ");
+            int n_231 = int.Parse(Console.ReadLine());
+            Console.WriteLine(result(i_231, n_231));
         }
 
-        private static double result(int i, int n)
+        private static double result(int i_231, int n_231)
         {
-            if (i <= n) return (1.0 / (2 * i)) + result(++i, n);
+            if (i_231 <= n_231) return (1.0 / (2 * i_231)) + result(++i_231, n_231);
             else return 0;
         }
     }
