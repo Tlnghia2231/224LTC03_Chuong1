713f33ae2cd5d62e52b55db6a1c53da272b287b7
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 22:06:02 +0700

    Demo bai 63

---
+++ Bt63/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BCNN cua 12 va 14 la : " + BCNN(12, 14));
+        }
+
+        private static int UCLN(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a);
+        }
+
+        static int BCNN(int a, int b)
+        {
+            return Math.Abs(a * b) / UCLN(a, b);
+        }
+    }
+}
