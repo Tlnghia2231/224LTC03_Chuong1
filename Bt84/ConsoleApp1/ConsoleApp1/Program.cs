72dff7489db1c3c3af8ec94f80a557fc199527ce
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 22:36:06 +0700

    Demo bai 84

---
+++ Bt84/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2x + 6  = 0");
+            result(2, 6);
+        }
+
+        private static void result(int a, int b)
+        {
+            if (a == 0)
+            {
+                if (b == 0) Console.WriteLine("Vo so nghiem");
+                else Console.WriteLine("Vo nghiem");
+            }
+            else Console.WriteLine(-b * 1.0 / a);
+        }
+    }
+}
