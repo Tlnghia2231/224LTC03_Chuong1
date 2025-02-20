5f8b2c9786700b97c557d6220cb4bedea391aed1
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 21:53:42 +0700

    Demo bt 54

---
+++ Bt54/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            result(4441);
+        }
+
+        private static void result(int n)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int min = int.MaxValue;
+            while (n > 0)
+            {
+                int tmp = n % 10;
+                if (!dict.ContainsKey(tmp)) { dict[tmp] = 0; }
+                dict[tmp]++;
+                if (min > tmp) min = tmp;
+                n /= 10;
+            }
+            Console.WriteLine("So be nhat la : " + min);
+            Console.WriteLine("Xuat hien : " + dict[min] + " lan");
+
+        }
+    }
+}
