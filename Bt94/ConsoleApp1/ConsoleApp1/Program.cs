7cadbb540115f135f9379f1e2b2f585a874b6dc7
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-20 22:43:57 +0700

    Demo bai 94

---
+++ Bt94/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cac so le nho hon 100 tru 5, 7, 93 la : ");
+            printNumber();
+        }
+
+        private static void printNumber()
+        {
+            int i = 1;
+            while (i < 100)
+            {
+                if (!(i == 5 || i == 7 || i == 93) && ((i & 1) == 1))
+                {
+                    Console.Write(i + ", ");
+                }
+                i++;
+            }
+        }
+    }
+}
