fa90abfd237f8f31d340a9c010cb499cbe96c124
Tlnghia2231 <22115053122231@sv.ute.udn.vn>
2025-02-21 20:43:35 +0700

    Hoan thanh bai 54

--- Bt54/ConsoleApp1/ConsoleApp1/Program.cs
+++ Bt54/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,23 +4,25 @@
     {
         static void Main(string[] args)
         {
-            result(4441);
+            Console.Write("Nhap so n : ");
+            int n_231 = int.Parse(Console.ReadLine());
+            result(n_231);
         }
 
-        private static void result(int n)
+        private static void result(int n_231)
         {
-            Dictionary<int, int> dict = new Dictionary<int, int>();
-            int min = int.MaxValue;
-            while (n > 0)
+            Dictionary<int, int> dict_231 = new Dictionary<int, int>();
+            int min_231 = int.MaxValue;
+            while (n_231 > 0)
             {
-                int tmp = n % 10;
-                if (!dict.ContainsKey(tmp)) { dict[tmp] = 0; }
-                dict[tmp]++;
-                if (min > tmp) min = tmp;
-                n /= 10;
+                int tmp = n_231 % 10;
+                if (!dict_231.ContainsKey(tmp)) { dict_231[tmp] = 0; }
+                dict_231[tmp]++;
+                if (min_231 > tmp) min_231 = tmp;
+                n_231 /= 10;
             }
-            Console.WriteLine("So be nhat la : " + min);
-            Console.WriteLine("Xuat hien : " + dict[min] + " lan");
+            Console.WriteLine("So be nhat la : " + min_231);
+            Console.WriteLine("Xuat hien : " + dict_231[min_231] + " lan");
 
         }
     }
--- Bt54/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Bt54/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3552f2af231856b7d98ec3bfe20a82e889ade4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8b2c9786700b97c557d6220cb4bedea391aed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
